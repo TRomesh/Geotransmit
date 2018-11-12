12154917670579b31d18a2808b656157fb81f545
TRomesh <tharakaromesh@gmail.com>
2018-11-12 12:37:16 +0530

    Add mongodb to server

--- Geo_Server/index.js
+++ Geo_Server/index.js
@@ -2,6 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors')();
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const mongoose = require('mongoose');
+const geo = require('./geoModel');
+//Set up default mongoose connection
+const mongoDB = 'mongodb://devx:devx123@ds259463.mlab.com:59463/geodb';
+mongoose.connect(mongoDB,{ useCreateIndex: true,useNewUrlParser: true });
+// Get Mongoose to use the global promise library
+mongoose.Promise = global.Promise;
+//Get the default connection
+const db = mongoose.connection;
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 
 const port =process.env.PORT || 4000;
 
@@ -13,12 +27,32 @@ app.use(bodyParser.urlencoded({
 app.use(cors);
 
 app.post('/addgeo',(req,res) => {
-    res.send("true");
+    var Geo = new geo({
+        package:req.body.package,
+        longitude: req.body.longitude,
+        lattitude: req.body.lattitude});
+    Geo.save(function(err){
+        if (err){
+            res.json( { status: 'ON' });
+        }else{
+            res.json( { status: 'OFF' });
+        }
+        
+    });
  });
 
 app.get('/getgeo',(req,res) => {
-    res.send("true");
+    geo.find({},function(err,data){
+        res.json(data);
+    })
+    
  });
 
+ io.on('connection', function (socket) {
+    socket.emit('light', { status: 'ON' });
+    socket.on('temp', function (data) {
+      console.log(data);
+    });
+  });
 
- app.listen(port,()=> console.log('Running on port: '+port));
+server.listen(port,()=> console.log('Running on port: '+port));
